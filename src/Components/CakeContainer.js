fbffc36f66268b1943aebc27e0dca7a0153b0415
rakshitha <rakshitha@robosoftin.com>
2022-03-12 18:25:20 +0530

    react redux iniatal setup

---
+++ src/Components/CakeContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function CakeContainer() {
+  return (
+      <>
+        <h1>No of Cakes</h1>
+        <button>Buy Cakes</button>
+      </>
+    
+  )
+}
---
+++ src/redux/store.js
@@ -0,0 +1,5 @@
+import { createStore } from "redux"
+import cakeReducer from "./Cake/cakeReducer"
+
+const store = createStore(cakeReducer);
+export default store;
